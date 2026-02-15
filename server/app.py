e40d451eaef9e58b070a9301247808117564e6e8
Zachary Mowatt <zamowatt@gmail.com>
2026-02-15 18:12:10 -0500

    final solution

--- server/app.py
+++ server/app.py
@@ -6,6 +6,8 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User, UserSchema
 
+user_schema = UserSchema()
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -15,7 +17,50 @@ class ClearSession(Resource):
 
         return {}, 204
 
+class Signup(Resource):
+    def post(self):
+        data = request.get_json() or {}
+
+        user = User(username=data.get('username'))
+        user.password_hash = data.get('password')
+
+        db.session.add(user)
+        db.session.commit()
+
+        session['user_id'] = user.id
+        return user_schema.dump(user), 201
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            if user:
+                return user_schema.dump(user), 200
+        return {}, 204
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json() or {}
+
+        user = User.query.filter(User.username == data.get('username')).first()
+
+        if user and user.authenticate(data.get('password')):
+            session['user_id'] = user.id
+            return user_schema.dump(user), 200
+
+        return {}, 401
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
